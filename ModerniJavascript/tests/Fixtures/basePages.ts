93c2872ec0382e59968c5757bb987438bb36fef9
Nikola Štěpánová <n.schebestikova@seznam.cz>
2026-01-12 14:30:54 +0100

    Restore all test files from previous commit

--- ModerniJavascript/tests/Fixtures/basePages.ts
+++ ModerniJavascript/tests/Fixtures/basePages.ts
@@ -1,19 +1,15 @@
-import { test as baseTest, Page } from '@playwright/test';
+import{test as baseTest} from '@playwright/test';
 
 import { HomePage } from '../page-object/homePage';
-import { LoginPage } from '../page-object/loginPage';
 
 const test = baseTest.extend<{
     homePage: HomePage;
-    loginPage: LoginPage;
 }>({
-    homePage: async ({ page }: { page: Page }, use: (homePage: HomePage) => Promise<void>) => {
-        await use(new HomePage(page));
-    },
-    loginPage: async ({ page }: { page: Page }, use: (loginPage: LoginPage) => Promise<void>) => {
-        await use(new LoginPage(page));
+    homePage: async ({ page }, use) => {
+        await use (new HomePage(page));
+
     }
 });
 
 export default test;
-export const { expect } = baseTest;
+export const {expect} = test;
